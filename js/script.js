cd40abdef2247d8f5b5278c36bbb44a7c76fe422
Bryan Jensen <bryanawjensen@gmail.com>
2014-11-30 22:41:13 -0500

    added structure for portfolio, removing old blog code from js

--- js/script.js
+++ js/script.js
@@ -1,177 +1,177 @@
-var blog = {
-	blogShowing: false,
-	blogEntryShowing: false,
+// var blog = {
+// 	blogShowing: false,
+// 	blogEntryShowing: false,
 
-	loadingImage: $('<img/>', {
-		id: 'blog-loading-gif',
-		src: '/images/loading.gif'
-	}),
+// 	loadingImage: $('<img/>', {
+// 		id: 'blog-loading-gif',
+// 		src: '/images/loading.gif'
+// 	}),
 
-	createDisplayTopicsCallback: function(topicList) {
-		var topicsRef = new Firebase('https://personal-blog.firebaseIO.com/topics/');
+// 	createDisplayTopicsCallback: function(topicList) {
+// 		var topicsRef = new Firebase('https://personal-blog.firebaseIO.com/topics/');
 
-		console.log('Assigning callback for topics.');
+// 		console.log('Assigning callback for topics.');
 
-		topicsRef.on('child_added', function(snapshot) {
-			console.log('Adding ' + snapshot.name());
-			dataJSON = snapshot.val();
+// 		topicsRef.on('child_added', function(snapshot) {
+// 			console.log('Adding ' + snapshot.name());
+// 			dataJSON = snapshot.val();
 
-			var newTopicLink = $('<li/>', {
-				id: snapshot.name(),
-				class: 'blog-topic-link',
-				html: dataJSON['display-name']
-			});
+// 			var newTopicLink = $('<li/>', {
+// 				id: snapshot.name(),
+// 				class: 'blog-topic-link',
+// 				html: dataJSON['display-name']
+// 			});
 
-			newTopicLink.click(topicLinkClickCallback);
-			topicList.append(newTopicLink);
-		});
-	},
+// 			newTopicLink.click(topicLinkClickCallback);
+// 			topicList.append(newTopicLink);
+// 		});
+// 	},
 
-	createTopicsList: function() {
-		var blogTopicsList = $('<ul/>', {
-			id: 'blog-options'
-		});
+// 	createTopicsList: function() {
+// 		var blogTopicsList = $('<ul/>', {
+// 			id: 'blog-options'
+// 		});
 
-		blogTopicsList.append(this.loadingGif);
+// 		blogTopicsList.append(this.loadingGif);
 
-		return blogTopicsList;
-	},
+// 		return blogTopicsList;
+// 	},
 
-	createBlogDiv: function() {
-		var blogDiv = $('<div/>', {
-			id: 'blog'
-		});
+// 	createBlogDiv: function() {
+// 		var blogDiv = $('<div/>', {
+// 			id: 'blog'
+// 		});
 
-		var blogTitle = $('<span/>', {
-			id: 'blog-title',
-			html: 'Welcome!'
-		});
+// 		var blogTitle = $('<span/>', {
+// 			id: 'blog-title',
+// 			html: 'Welcome!'
+// 		});
 
-		var blogGeneralDescription = $('<span/>', {
-			id: 'blog-general-description',
-			html: 'What do you want to read about?'
-		});
+// 		var blogGeneralDescription = $('<span/>', {
+// 			id: 'blog-general-description',
+// 			html: 'What do you want to read about?'
+// 		});
 
-		blogDiv.append(blogTitle);
-		blogDiv.append(blogGeneralDescription);
-		blogDiv.append(this.createTopicsList());
+// 		blogDiv.append(blogTitle);
+// 		blogDiv.append(blogGeneralDescription);
+// 		blogDiv.append(this.createTopicsList());
 
-		return blogDiv;
-	},
+// 		return blogDiv;
+// 	},
 
-	displayAfter: function(preceedingDiv) {
-		if (!this.blogShowing) {
-			var blogDiv = this.createBlogDiv();
+// 	displayAfter: function(preceedingDiv) {
+// 		if (!this.blogShowing) {
+// 			var blogDiv = this.createBlogDiv();
 
-			blogDiv.insertAfter(preceedingDiv);
+// 			blogDiv.insertAfter(preceedingDiv);
 
-			this.createDisplayTopicsCallback(blogDiv.find('#blog-options'));
-		}
+// 			this.createDisplayTopicsCallback(blogDiv.find('#blog-options'));
+// 		}
 
-		$('html, body').animate({
-			scrollTop: $('#blog').offset().top
-		}, 500);
+// 		$('html, body').animate({
+// 			scrollTop: $('#blog').offset().top
+// 		}, 500);
 
-		blogShowing = true;
-	}
-}
-
-
-function centerElements(elementsToCenter) {
-	// console.log('centering everything');
-	var transitionTime;
-
-	var promise = Promise.resolve(
-		$.each(elementsToCenter, function() {
-			console.log('centering ' + $(this).attr('id'));
-
-			console.log('outerWidth ' + $(this).outerWidth(true));
-
-			$(this).css('margin-left', -$(this).width() / 2);
-			$(this).css('margin-top', -$(this).height() / 2);
-		})
-	).catch(function(err) {
-		console.error(err);
-		throw err;
-	});
-}
-
-function displayPost() {
-
-	// $('.blog-topic-link').css('float', 'left');
-
-	var blogTextExists = $('#blog-text').length != 0;
-
-	if (!blogTextExists) {
-		var blogText = $('<div/>', {
-			id: 'blog-text'
-		});
-
-		$('#blog').append(blogText);
-	}
-	else {
-		$('#blog-text').empty();
-	}
-
-	var loadingImage = $('<img/>', {
-		id: 'blog-loading-gif',
-		src: 'images/loading.gif'
-	});
-
-	blogText.append(loadingImage);
+// 		blogShowing = true;
+// 	}
+// }
 
 
-	postsRef.on('child_added', function(snapshot) {
-		console.dir(snapshot.val());
-	});
+// function centerElements(elementsToCenter) {
+// 	// console.log('centering everything');
+// 	var transitionTime;
 
-}
+// 	var promise = Promise.resolve(
+// 		$.each(elementsToCenter, function() {
+// 			console.log('centering ' + $(this).attr('id'));
 
-function topicLinkClickCallback(clickEvent) {
-	// NOTE: "this" refers to the topic link clicked
-	displayTopicLinkPosts($(this), this.id);
-}
+// 			console.log('outerWidth ' + $(this).outerWidth(true));
 
-function displayTopicLinkPosts(containerListItem, topicID) {
-	console.log('Getting posts for ' + topicID);
-	var postsRef = new Firebase('https://personal-blog.firebaseIO.com/topics/' + topicID + '/posts/');
+// 			$(this).css('margin-left', -$(this).width() / 2);
+// 			$(this).css('margin-top', -$(this).height() / 2);
+// 		})
+// 	).catch(function(err) {
+// 		console.error(err);
+// 		throw err;
+// 	});
+// }
 
-	var subList = $('<ul/>', { class: 'blog-topic-post-list' });
+// function displayPost() {
 
-	containerListItem.append(subList);
+// 	// $('.blog-topic-link').css('float', 'left');
 
-	postsRef.on('child_added', function(snapshot) {
-		console.log('Adding post to ' + topicID);
-		var jsonObject = snapshot.val();
+// 	var blogTextExists = $('#blog-text').length != 0;
 
-		var newPost = $('<li/>', {
-			id: snapshot.name(),
-			class: 'snapshot-topic-post-link',
-			html: jsonObject['title']
-		});
+// 	if (!blogTextExists) {
+// 		var blogText = $('<div/>', {
+// 			id: 'blog-text'
+// 		});
 
-		subList.append(newPost);
-	});
+// 		$('#blog').append(blogText);
+// 	}
+// 	else {
+// 		$('#blog-text').empty();
+// 	}
 
-}
+// 	var loadingImage = $('<img/>', {
+// 		id: 'blog-loading-gif',
+// 		src: 'images/loading.gif'
+// 	});
 
-$(window).load(function() {
-	// var elementsToCenter = [
-	// 	$('#name'),
-	// 	// $('#linkedin-link'),
-	// 	// $('#resume-link'),
-	// 	// $('#github-link'),
-	// 	// $('#portfolio-link'),
-	// 	// $('#blog-link')
-	// 	$('#links')
-	// ];
+// 	blogText.append(loadingImage);
 
-	// centerElements(elementsToCenter);
-});
 
-$(function() {
-	$('#blog-link').click(function(event) {
-		event.preventDefault();
-		blog.displayAfter($('#content'));
-	});
-});
+// 	postsRef.on('child_added', function(snapshot) {
+// 		console.dir(snapshot.val());
+// 	});
+
+// }
+
+// function topicLinkClickCallback(clickEvent) {
+// 	// NOTE: "this" refers to the topic link clicked
+// 	displayTopicLinkPosts($(this), this.id);
+// }
+
+// function displayTopicLinkPosts(containerListItem, topicID) {
+// 	console.log('Getting posts for ' + topicID);
+// 	var postsRef = new Firebase('https://personal-blog.firebaseIO.com/topics/' + topicID + '/posts/');
+
+// 	var subList = $('<ul/>', { class: 'blog-topic-post-list' });
+
+// 	containerListItem.append(subList);
+
+// 	postsRef.on('child_added', function(snapshot) {
+// 		console.log('Adding post to ' + topicID);
+// 		var jsonObject = snapshot.val();
+
+// 		var newPost = $('<li/>', {
+// 			id: snapshot.name(),
+// 			class: 'snapshot-topic-post-link',
+// 			html: jsonObject['title']
+// 		});
+
+// 		subList.append(newPost);
+// 	});
+
+// }
+
+// $(window).load(function() {
+// 	// var elementsToCenter = [
+// 	// 	$('#name'),
+// 	// 	// $('#linkedin-link'),
+// 	// 	// $('#resume-link'),
+// 	// 	// $('#github-link'),
+// 	// 	// $('#portfolio-link'),
+// 	// 	// $('#blog-link')
+// 	// 	$('#links')
+// 	// ];
+
+// 	// centerElements(elementsToCenter);
+// });
+
+// $(function() {
+// 	$('#blog-link').click(function(event) {
+// 		event.preventDefault();
+// 		blog.displayAfter($('#content'));
+// 	});
+// });
