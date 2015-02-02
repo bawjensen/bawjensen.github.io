5882467c8378c7d4e4f0c2cf795f0ed9f6855da1
Bryan Jensen <bryanawjensen@gmail.com>
2015-02-01 22:40:17 -0500

    Adding changes from before

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,79 @@
 var CURRENT_ROUTE = window.location.pathname;
 
-if (CURRENT_ROUTE.match('portfolio')) {
-    $(function() {
-        $('#project-list li').click(function() {
-            var targetId = $(this).attr('id');
-            var newId = '#' + targetId + '-info';
-            $('#project-info > div').hide();
 
-            var $newDiv = $(newId);
+// ==================================== Portfolio scripts ============================
 
-            $newDiv.show().parent('#project-info').height($newDiv.height());
-        });
+function initializePortfolio() {
+    $('#project-list li').click(function() {
+        var targetId = $(this).attr('id');
+        var newId = '#' + targetId + '-info';
+        $('#project-info > div').hide();
+
+        var $newDiv = $(newId);
+
+        $newDiv.show().parent('#project-info').height($newDiv.height());
     });
 }
+
+// ==================================== Main page scripts ============================
+
+// ------------------------------------ Blog scripts ---------------------------------
+
+/*
+
+Blog structure:
+
+Blog
+\_ Topic (i.e. Ramblings, Tech, Snippets)
+    \_ Post ('What I love about Programming', etc.)
+
+*/
+
+var blog = {};
+function BlogState() {
+    this.possibleTransitions = [];
+
+    this.possibleTransitions.push({
+        transition: function() {
+
+        },
+        destination: null
+    });
+}
+
+blog.handle = function(subRoute) {
+    console.log(subRoute);
+}
+
+function handleHashChange() {
+    var newHash = location.hash;
+
+    newHash = newHash.replace(/^#/, '');
+
+    var hashParts = newHash.split('/');
+
+    if (hashParts[0] == 'blog') {
+        blog.handle(hashParts.slice(1).join('/'));
+    }
+}
+
+// ------------------------------------ Other scripts --------------------------------
+
+function initializeMainPage() {
+    window.onhashchange = handleHashChange;
+
+    if (location.hash) handleHashChange();
+
+    blog.state = null;
+}
+
+// ==================================== OnLoad scripts ===============================
+
+$(function() {
+    if (CURRENT_ROUTE.match('portfolio')) {
+        initializePortfolio();
+    }
+    else if (CURRENT_ROUTE.match('/')) {
+        initializeMainPage();
+    }
+});
