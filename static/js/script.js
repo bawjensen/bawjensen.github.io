c00c6fff0c22a06379ba02e4e8e5e4533e23b51d
Bryan Jensen <bryanawjensen@gmail.com>
2015-02-17 13:15:13 -0500

    Removing behavior of blog

--- static/js/script.js
+++ static/js/script.js
@@ -16,167 +16,150 @@ function initializePortfolio() {
 
 // ==================================== Main page scripts ============================
 
-/*
+// function scrollToBlog() {
+//     $('html, body').animate({
+//         scrollTop: $('#blog-container').offset().top
+//     }, 250);
+// }
 
-Blog layers
-
-Blog
-\_ Topic (i.e. Ramblings, Tech, Snippets)
-    \_ Post ('What I love about Programming', etc.)
-
-*/
-
-function scrollToBlog() {
-    $('html, body').animate({
-        scrollTop: $('#blog-container').offset().top
-    }, 250);
-}
-
-var blog = {};
+// var blog = {};
 
 function initializeMainPage() {
-    $(document).on('click', '.state-transition', function(evt) {
-        var transitionId = $(this).data('state');
+    // $(document).on('click', '.state-transition', function(evt) {
+    //     var transitionId = $(this).data('state');
 
-        if (transitionId in blog.state.transitions) {
-            blog.state.transitions[transitionId](evt);
-        }
-    });
+    //     if (transitionId in blog.state.transitions) {
+    //         blog.state.transitions[transitionId](evt);
+    //     }
+    // });
 
-    var topics = new Firebase("https://personal-blog.firebaseio.com/");
+    // var topics = new Firebase("https://personal-blog.firebaseio.com/");
 
-    topics.on("value", function(data) {
-        var topicsObj = data.val() ? data.val().topics : {};
+    // topics.on("value", function(data) {
+    //     var topicsObj = data.val() ? data.val().topics : {};
 
-        var topicLinksTemplate = blogTemplate({ topicsObj: topicsObj });
+    //     var topicLinksTemplate = blogTemplate({ topicsObj: topicsObj });
 
-        var topicHtmls = {};
-        for (var key in topicsObj) {
-            topicHtmls[key] = topicTemplate({ topic: topicsObj[key] });
-        }
+    //     var topicHtmls = {};
+    //     for (var key in topicsObj) {
+    //         topicHtmls[key] = topicTemplate({ topic: topicsObj[key] });
+    //     }
 
-        blog.layers = {
-            'first': {
-                transitions: {
-                    'intro': function(evt) {
-                        var $newDiv = $($.parseHTML(topicLinksTemplate));
+    //     blog.layers = {
+    //         'first': {
+    //             transitions: {
+    //                 'intro': function(evt) {
+    //                     var $newDiv = $($.parseHTML(topicLinksTemplate));
 
-                        $newDiv.appendTo('#content');
+    //                     $newDiv.appendTo('#content');
 
-                        scrollToBlog();
+    //                     scrollToBlog();
 
-                        blog.state = blog.layers.second;
-                    }
-                }
-            },
-            'second': {
-                transitions: {
-                    'home': function(evt) {
-                        $('#blog-container').remove();
-                        blog.state = blog.layers.first;
-                    },
-                    'intro': scrollToBlog,
-                    'topic': function(evt) {
-                        var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
+    //                     blog.state = blog.layers.second;
+    //                 }
+    //             }
+    //         },
+    //         'second': {
+    //             transitions: {
+    //                 'home': function(evt) {
+    //                     $('#blog-container').remove();
+    //                     blog.state = blog.layers.first;
+    //                 },
+    //                 'intro': scrollToBlog,
+    //                 'topic': function(evt) {
+    //                     var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
 
-                        var topics = $('#topics-container');
+    //                     var topics = $('#topics-container');
 
-                        // Manually set the margin, make it floated, and display block (removes funkiness with animation)
-                        topics.css('margin-left', topics.css('margin-left'));
-                        topics.css('float', 'left');
-                        topics.css('display', 'block');
+    //                     // Manually set the margin, make it floated, and display block (removes funkiness with animation)
+    //                     topics.css('margin-left', topics.css('margin-left'));
+    //                     topics.css('float', 'left');
+    //                     topics.css('display', 'block');
 
-                        topics.animate({
-                            marginLeft: 40,
-                            marginRight: 40,
-                            // fontSize: 30,
-                            // borderRightColor: 'white',
-                            // borderRightWidth: 1,
-                            width: 200
-                        }, 250, function after() {
-                            // topics.css('line-height', topics.css('line-height'));
-                            // topics.css('font-size', '20px');
-                            $newDiv.appendTo($('#blog-container'));
-                        });
+    //                     topics.animate({
+    //                         marginLeft: 40,
+    //                         marginRight: 40,
+    //                         // fontSize: 30,
+    //                         // borderRightColor: 'white',
+    //                         // borderRightWidth: 1,
+    //                         width: 200
+    //                     }, 250, function after() {
+    //                         // topics.css('line-height', topics.css('line-height'));
+    //                         // topics.css('font-size', '20px');
+    //                         $newDiv.appendTo($('#blog-container'));
+    //                     });
 
-                        blog.state = blog.layers.third;
-                    }
-                }
-            },
-            'third': {
-                transitions: {
-                    'home': function(evt) {
-                        $('#blog-container').remove();
-                        blog.state = blog.layers.first;
-                    },
-                    'intro': scrollToBlog,
-                    'topic': function(evt) {
-                        var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
+    //                     blog.state = blog.layers.third;
+    //                 }
+    //             }
+    //         },
+    //         'third': {
+    //             transitions: {
+    //                 'home': function(evt) {
+    //                     $('#blog-container').remove();
+    //                     blog.state = blog.layers.first;
+    //                 },
+    //                 'intro': scrollToBlog,
+    //                 'topic': function(evt) {
+    //                     var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
 
-                        $('#posts').replaceWith($newDiv);
-                    },
-                    'post': function(evt) {
-                        var $clicked = $(evt.target);
+    //                     $('#posts').replaceWith($newDiv);
+    //                 },
+    //                 'post': function(evt) {
+    //                     var $clicked = $(evt.target);
 
-                        $clicked.closest('.post').find('.preview, .text').toggle();
+    //                     $clicked.closest('.post').find('.preview, .text').toggle();
 
-                        blog.state = blog.layers.fourth;
-                        blog.state.$showing = $clicked;
-                    },
-                    'close': function(evt) {
-                        $('#posts').remove();
-                        $('#topics-container').attr('style', '');
-                        blog.state = blog.layers.second;
-                    }
-                }
-            },
-            'fourth': {
-                transitions: {
-                    'home': function(evt) {
-                        $('#blog-container').remove();
-                        blog.state = blog.layers.first;
-                    },
-                    'intro': scrollToBlog,
-                    'topic': function(evt) {
-                        var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
+    //                     blog.state = blog.layers.fourth;
+    //                     blog.state.$showing = $clicked;
+    //                 },
+    //                 'close': function(evt) {
+    //                     $('#posts').remove();
+    //                     $('#topics-container').attr('style', '');
+    //                     blog.state = blog.layers.second;
+    //                 }
+    //             }
+    //         },
+    //         'fourth': {
+    //             transitions: {
+    //                 'home': function(evt) {
+    //                     $('#blog-container').remove();
+    //                     blog.state = blog.layers.first;
+    //                 },
+    //                 'intro': scrollToBlog,
+    //                 'topic': function(evt) {
+    //                     var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
 
-                        $('#posts').replaceWith($newDiv);
-                    },
-                    'post': function(evt) {
-                        var $clicked = $(evt.target).closest('.post');
+    //                     $('#posts').replaceWith($newDiv);
+    //                 },
+    //                 'post': function(evt) {
+    //                     var $clicked = $(evt.target).closest('.post');
 
-                        $clicked.find('.preview, .text').toggle();
+    //                     $clicked.find('.preview, .text').toggle();
 
-                        if (blog.state.$showing && ($clicked[0] == blog.state.$showing[0])) {
-                            blog.state = blog.layers.third;
-                        }
-                        else if (blog.state.$showing) {
-                            blog.state.$showing.find('.preview, .text').toggle();
-                            blog.state.$showing = $clicked;
-                        }
-                        else {
-                            blog.state.$showing = $clicked;
-                        }
-                    },
-                    'close': function(evt) {
-                        $('#posts').remove();
-                        $('#topics-container').attr('style', '');
-                        blog.state = blog.layers.second;
-                    }
-                },
-                $showing: null
-            }
-        };
+    //                     if (blog.state.$showing && ($clicked[0] == blog.state.$showing[0])) {
+    //                         blog.state = blog.layers.third;
+    //                     }
+    //                     else if (blog.state.$showing) {
+    //                         blog.state.$showing.find('.preview, .text').toggle();
+    //                         blog.state.$showing = $clicked;
+    //                     }
+    //                     else {
+    //                         blog.state.$showing = $clicked;
+    //                     }
+    //                 },
+    //                 'close': function(evt) {
+    //                     $('#posts').remove();
+    //                     $('#topics-container').attr('style', '');
+    //                     blog.state = blog.layers.second;
+    //                 }
+    //             },
+    //             $showing: null
+    //         }
+    //     };
 
-        // for 
-
-        blog.state = blog.layers.first;
-
-        // for (var topic in topicsObj) {
-        //     blog.state.transitions[topic] = function() {
-        //     };
-        // }
-    });
+    //     blog.state = blog.layers.first;
+    // });
 }
 
 // ==================================== OnLoad scripts ===============================
