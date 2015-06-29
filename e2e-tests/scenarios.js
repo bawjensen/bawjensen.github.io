902d6866ec988fbeaaf3ccb10e0332f77d97b86c
Bryan Jensen <bryanawjensen@gmail.com>
2015-06-28 21:29:23 -0400

    Messing around with e2e tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,41 +2,50 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('personal site', function() {
+  it('should render home.html when connecting to base site', function() {
+    browser.get('/');
+    var test = element.all(by.css('[ng-scope] #home-container')).first().getText();
+
+    console.log('test:', test);
+
+    expect(test)
+      .not.toBe(null);
+  })
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
+  // it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  //   browser.get('/');
+  //   expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  // });
 
 
-  describe('view1', function() {
+  // describe('view1', function() {
 
-    beforeEach(function() {
-      browser.get('index.html#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
+  //   beforeEach(function() {
+  //     browser.get('index.html#/view1');
+  //   });
 
 
-  describe('view2', function() {
+  //   it('should render view1 when user navigates to /view1', function() {
+  //     expect(element.all(by.css('[ng-view] p')).first().getText()).
+  //       toMatch(/partial for view 1/);
+  //   });
 
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
+  // });
 
 
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
+  // describe('view2', function() {
 
-  });
+  //   beforeEach(function() {
+  //     browser.get('index.html#/view2');
+  //   });
+
+
+  //   it('should render view2 when user navigates to /view2', function() {
+  //     expect(element.all(by.css('[ng-view] p')).first().getText()).
+  //       toMatch(/partial for view 2/);
+  //   });
+
+  // });
 });
