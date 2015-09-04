4bed84f530850e2ac7c5e839dbf962cbfd5d8d04
Bryan Jensen <bryanawjensen@gmail.com>
2015-09-04 14:05:03 -0700

    Chaning ga tracking, minor portfolio fixes, etc

--- js/app.js
+++ js/app.js
@@ -7,29 +7,36 @@ angular.module('personalSite', [
   'ngResource'
 ])
 
-.factory('Portfolio', ['$resource',
+.factory('Portfolio',
   function($resource) {
-    return $resource('json/portfolio.json', { cache: true }, {
+    return $resource('json/portfolio.json', {}, {
       query: { method:'GET', isArray: true }
     });
-  }]
+  }
 )
 
-.controller('PortfolioListCtrl', ['$scope', 'Portfolio',
+.controller('SiteWideCtrl',
+  function($scope, $window, $location, Portfolio) {
+    $scope.$on('$locationChangeStart', function(event) {
+      $window.ga('send', 'pageview', { page: $location.url() });
+    });
+  }
+)
+.controller('PortfolioListCtrl',
   function($scope, Portfolio) {
     $scope.categories = Portfolio.query();
+
     $scope.toggle = function(category) {
       category.visible = !category.visible;
-      console.log(category.visible);
     };
-  }]
+  }
 )
-.controller('PortfolioDetailCtrl', ['$scope', '$stateParams', 'Portfolio',
+.controller('PortfolioDetailCtrl',
   function ($scope, $stateParams, Portfolio) {
     $scope.category = $stateParams.category;
     $scope.entryId = $stateParams.entryId;
     $scope.entries = Portfolio.query();
-  }]
+  }
 )
 
 
@@ -37,37 +44,37 @@ angular.module('personalSite', [
 .config(function($stateProvider, $urlRouterProvider) {
 
   // For any unmatched url, redirect to / (a.k.a. home)
-  // $urlRouterProvider.otherwise("/");
+  $urlRouterProvider.otherwise('/');
 
   // Now set up the states
   $stateProvider
     .state('home', {
-      url: "",
-      templateUrl: "partials/home.html"
+      url: '',
+      templateUrl: 'partials/home.html'
     })
 
 
     .state('not-home', {
-      url: "",
-      templateUrl: "partials/not-home.html"
+      url: '',
+      templateUrl: 'partials/not-home.html'
     })
 
 
     .state('not-home.resume', {
-      url: "/resume",
-      templateUrl: "partials/resume.html"
+      url: '/resume',
+      templateUrl: 'partials/resume.html'
     })
 
 
     .state('not-home.portfolio', {
-      url: "/portfolio",
-      templateUrl: "partials/portfolio.html",
+      url: '/portfolio',
+      templateUrl: 'partials/portfolio.html',
       controller: 'PortfolioListCtrl'
     })
 
     .state('not-home.portfolio.detail', {
-      url: "/:category/:entryId",
-      templateUrl: "partials/portfolioEntry.html",
+      url: '/:category/:entryId',
+      templateUrl: 'partials/portfolioEntry.html',
       controller: 'PortfolioDetailCtrl'
     });
 });
