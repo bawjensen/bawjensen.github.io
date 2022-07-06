6bafab6d8ff6c4c0ba07d49d0d902d05efeaea1f
Bryan Jensen <bryan@thumbtack.com>
2022-07-05 18:21:10 -0700

    Restore favicon functionality with leftover favicon image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import React from "react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 import "./app.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Bryan Jensen</title>
+
+                <link rel="icon" href="/images/favicon.jpg" />
+            </Head>
+
+            <Component {...pageProps} />
+        </React.Fragment>
+    );
 }
